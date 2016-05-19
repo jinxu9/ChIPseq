a8715cab6aebe9aa95fbd0a20495f5ec810bff17
jinxu9 <jinxu9@users.noreply.github.com>
2016-05-19 12:01:20 -0700

    Create PeakCalling.sh

---
+++ PeakCalling.sh
@@ -0,0 +1,2 @@
+macs2  callpeak -t /home/jinxu/Backup/Rai1-KO/Rai1-Chip-second/Rai1_ChIP_A/Rai1_ChIP_A.pe.q10.rmdup.bam -c /home/jinxu/Backup/Rai1-KO/Rai1-Chip-second/Rai1_Input_A/Rai1_Input_A.pe.q10.rmdup.bam  -n Rai-Ch
+ip_A  -f BAM -g hs  --outdir ./  -p 0.001   --fix-bimodal   
